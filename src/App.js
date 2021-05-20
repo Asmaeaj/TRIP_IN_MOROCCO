19a8e05940eefb4b593b2c6f9e940317be4bccac
unknown <Asmaeaj>
2021-05-20 22:15:38 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import Home from './pages/Home';
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" exact>
+          <About />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "../style/CSS/about.css";
+
+class About extends Component {
+  render() {
+    return <React.Fragment>Hello!</React.Fragment>;
+  }
+}
+export default About;
